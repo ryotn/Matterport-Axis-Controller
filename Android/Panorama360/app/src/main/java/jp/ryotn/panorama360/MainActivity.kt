c5f3255db1f9891b1dfeee563b25575ef33a3740
ryotn <kh2oukusyon@gmail.com>
2023-10-27 01:24:43 +0900

    とりあえず接続して動くところまで

--- Android/Panorama360/app/src/main/java/jp/ryotn/panorama360/MainActivity.kt
+++ Android/Panorama360/app/src/main/java/jp/ryotn/panorama360/MainActivity.kt
@@ -2,18 +2,84 @@ package jp.ryotn.panorama360
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.WindowManager
 import android.widget.Button
+import android.widget.TextView
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     private lateinit var mMatterportAxisManager: MatterportAxisManager
+    private lateinit var mTextState: TextView
+    private lateinit var mTextAngle: TextView
+    private lateinit var mBtnConnect: Button
+    private lateinit var mBtnTestBtn: Button
+    private lateinit var mBtnResetAngle: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
         mMatterportAxisManager = MatterportAxisManager(context = this)
 
-        val btn = findViewById<Button>(R.id.Scan)
-        btn.setOnClickListener {
-            mMatterportAxisManager.connect()
+        mTextState = findViewById(R.id.txtState)
+        mTextAngle = findViewById(R.id.txtAngle)
+        mTextAngle.text = getString(R.string.angle,0)
+        mBtnTestBtn = findViewById(R.id.btnSetAngle)
+        mBtnResetAngle = findViewById(R.id.btnReset)
+
+        mBtnConnect = findViewById(R.id.btnConnect)
+        mBtnConnect.setOnClickListener {
+            if (mMatterportAxisManager.isConnected()) {
+                mMatterportAxisManager.disconnect()
+                mBtnConnect.text = getString(R.string.connect)
+            } else {
+                mMatterportAxisManager.connect()
+                mBtnConnect.text = getString(R.string.connecting)
+            }
+            mBtnConnect.isEnabled = false
         }
+
+        mBtnTestBtn.setOnClickListener {
+            mMatterportAxisManager.sendAngle(10u)
+        }
+
+        mBtnResetAngle.setOnClickListener {
+            mMatterportAxisManager.resetAngle()
+            mBtnResetAngle.isEnabled = false
+            Handler(Looper.getMainLooper()).postDelayed( {
+                mBtnResetAngle.isEnabled = true
+            }, 1000)
+        }
+
+        mMatterportAxisManager.mListener = mMatterportAxisManagerListener
+    }
+
+    private val mMatterportAxisManagerListener = object : MatterportAxisManager.MatterportAxisManagerListener {
+        override fun connected() {
+            GlobalScope.launch(Dispatchers.Main) {
+                mBtnConnect.text = getString(R.string.disconnect)
+                mTextState.text = getString(R.string.state_connected)
+                mBtnConnect.isEnabled = true
+            }
+        }
+
+        override fun disconnected() {
+            GlobalScope.launch(Dispatchers.Main) {
+                mBtnConnect.text = getString(R.string.connect)
+                mTextState.text = getString(R.string.state_disconnected)
+                mBtnConnect.isEnabled = true
+            }
+        }
+
+        override fun receiveAngle() {
+            GlobalScope.launch(Dispatchers.Main) {
+                val angle = mMatterportAxisManager.getAngle()
+                mTextAngle.text = getString(R.string.angle, angle)
+            }
+        }
+
     }
 }
