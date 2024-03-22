6843322b9aab7b3ff66da8e91997872e8f5d28b2
RyoTN <13426723+ryotn@users.noreply.github.com>
2024-03-22 23:13:02 +0900

    Viewの追加

---
+++ iOS/Panorama360_MatterportAxis/Panorama360_MatterportAxis/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  Panorama360_MatterportAxis
+//
+//  Created by RyoTN on 2024/03/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ContentView()
+}
